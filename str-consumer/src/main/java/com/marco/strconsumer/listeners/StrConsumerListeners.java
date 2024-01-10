eed578cbb2b176ef782f60b65426a0f4416bcc99
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-10 15:19:14 -0300

    Implementando interceptor para os registros

--- str-consumer/src/main/java/com/marco/strconsumer/listeners/StrConsumerListeners.java
+++ str-consumer/src/main/java/com/marco/strconsumer/listeners/StrConsumerListeners.java
@@ -20,7 +20,7 @@ public class StrConsumerListeners {
         log.info("LOG ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2",topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Receive message {}", message);
     }
