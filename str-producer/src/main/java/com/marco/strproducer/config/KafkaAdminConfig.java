48974dcdc4bfdc092fff4444777453929db81b66
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-10 14:49:09 -0300

    Implementando Listener Customizado

--- str-producer/src/main/java/com/marco/strproducer/config/KafkaAdminConfig.java
+++ str-producer/src/main/java/com/marco/strproducer/config/KafkaAdminConfig.java
@@ -26,6 +26,6 @@ public final KafkaProperties properties;
 
     @Bean
     public KafkaAdmin.NewTopics topics(){
-    return new KafkaAdmin.NewTopics(TopicBuilder.name("str-topic").partitions(2).replicas(1).build());
+    return new KafkaAdmin.NewTopics(TopicBuilder.name("str-topic").partitions(2).replicas(1).build() );
     }
 }
